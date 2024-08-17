4122888bf8562d0d59e90cd9504c7c71250d3abe
ZombieNW <59242209+ZombieNW@users.noreply.github.com>
2024-08-17 15:55:16 -0700

    Components

---
+++ src/components/Article.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let title = "article title";
+    export let timestamp = new Date();
+    export let image = "./placeholder.jpg";
+    if(!image) image = "./placeholder.jpg";
+</script>
+
+<div class="w-[23.5rem] h-72 float-left my-2">
+    <div class="block">
+        <img src="{image}" class="h-48 w-full rounded-sm object-cover" alt="Placeholder">
+    </div>
+    <div class="block">
+        <h1 class="text-2xl leading-7 line-clamp-3 text-ellipsis">{title}</h1>
+        <p class="text-gray-700">{new Date(timestamp).toLocaleDateString()}</p>
+    </div>
+</div>
---
+++ src/components/SmallArticle.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let title = "article title";
+    export let timestamp = new Date();
+    export let image = "./placeholder.jpg";
+    if(!image) image = "./placeholder.jpg";
+</script>
+
+<div class="w-auto md:w-60 h-64 float-right mb-4 mt-1">
+    <div class="block">
+        <img src="{image}" class="h-40 w-full rounded-sm object-cover" alt="Placeholder">
+    </div>
+    <div class="block">
+        <h1 class="text-lg leading-6 line-clamp-4 text-ellipsis">{title}</h1>
+        <p class="text-gray-700">{new Date(timestamp).toLocaleDateString()}</p>
+    </div>
+</div>
