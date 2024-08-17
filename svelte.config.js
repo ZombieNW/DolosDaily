931c18e4e64e8b64e377a79f0a899444cb398be1
ZombieNW <59242209+ZombieNW@users.noreply.github.com>
2024-08-17 16:12:58 -0700

    Prepare for Github Pages

--- svelte.config.js
+++ svelte.config.js
@@ -8,10 +8,13 @@ const config = {
             // these options are set automatically — see below
             pages: "build",
             assets: "build",
-            fallback: undefined,
+            fallback: "404.html",
             precompress: false,
             strict: true
-        })
+        }),
+        paths: {
+            base: process.argv.includes("dev") ? "" : process.env.BASE_PATH
+        }
     },
     preprocess: vitePreprocess()
 };
